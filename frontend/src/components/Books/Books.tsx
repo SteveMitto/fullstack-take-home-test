0669e1c04b5bdd9000104f4475ad0e318b303ad3
Steve Mitto <mittosteve@gmail.com>
2024-06-08 11:09:18 +0300

    initializing Books Component

---
+++ frontend/src/components/Books/Books.tsx
@@ -0,0 +1,20 @@
+import { Box, Container } from '@mui/material'
+import React from 'react'
+import request, { gql } from 'graphql-request'
+import { useQuery } from '@tanstack/react-query'
+
+
+function Books() {
+  return (
+    <Container>
+        <Box>
+
+        </Box>
+        <Box>
+
+        </Box>
+    </Container>
+  )
+}
+
+export default Books
