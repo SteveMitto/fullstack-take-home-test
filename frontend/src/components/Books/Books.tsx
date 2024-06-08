265ee485060c8c0fa9f62e90943e430f60144917
Steve Mitto <mittosteve@gmail.com>
2024-06-08 23:44:39 +0300

    Adding paggination to Books display

--- frontend/src/components/Books/Books.tsx
+++ frontend/src/components/Books/Books.tsx
@@ -1,8 +1,9 @@
-import { Box, Stack, TextField } from '@mui/material'
+import { Box, Pagination, Skeleton, Slide, Stack, TextField, useScrollTrigger } from '@mui/material'
 import React, { useState, useEffect } from 'react'
 import request, { gql } from 'graphql-request'
 import { useQuery } from '@tanstack/react-query'
 import Grid from '@mui/material/Unstable_Grid2/Grid2';
+
 import BookCard from './Card';
 
 interface BookType {
@@ -24,25 +25,39 @@ const allBooks = gql`
 
 function Books() {
   const [searchTerm, setSearchTerm] = useState<string>('')
+  const [pageSize, setPageSize] = React.useState(0);
+  const [page, setPage] = React.useState(1);
   const [books, setBooks] = useState<BookType[] | []>([])
   const [filteredBooks, setFilteredBooks] = useState<BookType[] | []>([])
+  const trigger = useScrollTrigger();
   const { data }: { data: any } = useQuery({
     queryKey: ['books'],
     queryFn: async () => request('http://localhost:4000/books', allBooks)
   })
 
-  useEffect(()=>{
-    if (data) setBooks(data.books)
-  },[data])
+  useEffect(() => {
+    if (data) {
+      setBooks(data.books.slice(0, 10))
+      const pageSizeCalc = data.books.length / 10
+      setPageSize(data.books.length / 10)
+    }
 
-  useEffect(()=>{
-    if(searchTerm && data.books) {
-      const foundBooks = data.books.filter( book => book.title.toLowerCase().includes(searchTerm.toLowerCase()))
+  }, [data])
+
+  useEffect(() => {
+    if (searchTerm && data.books) {
+      const foundBooks = data.books.filter(book => book.title.toLowerCase().includes(searchTerm.toLowerCase()))
       setFilteredBooks(foundBooks)
     }
-  },[data,searchTerm])
+  }, [data, searchTerm])
+
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+    setBooks(data.books.slice((value - 1) * pageSize, (value * pageSize)))
+    document.getElementById("main")?.scrollTo(0, 0)
+  }
   return (
-    <Stack width="100%" spacing={2} maxHeight={'100vh'}>
+    <Stack position="relative" width="100%" spacing={2} maxHeight={'100vh'}>
       <Box bgcolor="#FFFFFF" p={3} position="fixed" zIndex={10} width="100%" top="0px" display="flex" gap="10%">
         <Box fontWeight={700} fontSize={40} color={(theme: any) => theme.primary.steelBlue}>Books</Box>
         <Box width="50%" display="flex" alignItems='center'>
@@ -52,14 +67,22 @@ function Books() {
             fullWidth id="input-with-sx" label="Find a book" size='small' variant="standard" />
         </Box>
       </Box>
-      <Box pt={13} px={3}>
+      <Box pt={13} px={3} sx={{overflowY:"scroll"}} id="main">
         <Grid container spacing={3}>
-          { 
-          books  
-          ?<BookCard books={filteredBooks.length > 0 ? filteredBooks : books} />
-          : <></>
+          {
+            books
+              ? <BookCard books={filteredBooks.length > 0 ? filteredBooks : books} />
+              : <></>
           }
         </Grid>
+        {/* <Slide in={trigger} direction='up' timeout={{exit:1500}} > */}
+          {/* <Box position="fixed" bottom="40px" width="80%" display="flex" justifyContent="center" alignItems="center"> */}
+          <Box  my={5} width="100%" display="flex" justifyContent="center" alignItems="center">
+            <Box bgcolor="white" boxSizing="border-box" display="block" borderRadius="20px">
+              <Pagination count={pageSize} color="primary" page={page} onChange={handleChange} />
+            </Box>
+          </Box>
+        {/* </Slide> */}
       </Box>
     </Stack>
   )
